76b406bf804da65384b637a9ed89c5a86b8264e3
Edouard Misset <edouardmisset@gmail.com>
2021-04-12 12:08:24 +0200

    working CRA + fetch + display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import DisplayEmployee from './components/DisplayEmployee';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const sampleEmployee = {
+  gender: 'male',
+  name: {
+    first: 'Charlie',
+    last: 'Thompson',
+  },
+  location: {
+    street: {
+      number: 761,
+      name: 'Tay Street',
+    },
+    city: 'Timaru',
+    postcode: 76111,
+  },
+  email: 'charlie.thompson@example.com',
+  picture: {
+    medium: 'https://randomuser.me/api/portraits/med/men/40.jpg',
+  },
+};
+
 function App() {
+  const [employee, setEmployee] = useState(sampleEmployee);
+
+  // * Using axios
+  // const getEmployee = () =>
+  //   axios
+  //     .get('https://randomuser.me/api?nat=en')
+  //     .then(resp => resp.data)
+  //     .then(data => setEmployee(data.results[0]));
+
+  // * Using default fetch method
+  const getEmployee2 = () =>
+    fetch('https://randomuser.me/api?nat=en')
+      .then(res => res.json())
+      .then(employeeList => setEmployee(employeeList.results[0]))
+      .catch(err => console.log(err));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <DisplayEmployee employee={employee} />
+      <button type='button' onClick={getEmployee2}>
+        Get employee
+      </button>
+    </>
   );
 }
 
